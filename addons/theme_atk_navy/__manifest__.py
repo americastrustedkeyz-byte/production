7f8b7d477a90ce89611ce280577cd9c4258698de
gracecreatives <gracecreatives302@gmail.com>
2026-01-09 00:48:46 +0100

    theme manifest updated for display

--- addons/theme_atk_navy/__manifest__.py
+++ addons/theme_atk_navy/__manifest__.py
@@ -4,8 +4,14 @@
     "category": "Theme/Website",
     "summary": "America's Trusted Key - Navy Standard Theme",
     "depends": ["website"],
-    "data": [],
-    "assets": {},
+    "data": [
+        "views/assets.xml",
+    ],
+    "assets": {
+        "website.assets_frontend": [
+            "theme_atk_navy/static/src/scss/atk_theme.scss",
+        ],
+    },
     "theme": True,
     "installable": True,
     "application": False,
