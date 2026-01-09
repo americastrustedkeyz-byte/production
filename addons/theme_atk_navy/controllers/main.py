898a7c4c70a17fa360d233fd4d56fe2dd46895c8
gracecreatives <gracecreatives302@gmail.com>
2026-01-09 23:10:08 +0100

    Fix homepage rendering by binding views to active website context

--- addons/theme_atk_navy/controllers/main.py
+++ addons/theme_atk_navy/controllers/main.py
@@ -3,7 +3,6 @@ from odoo.http import request
 from datetime import datetime
 import pytz
 
-
 class ATKLaunchController(http.Controller):
 
     def _launch_is_open(self):
@@ -16,14 +15,24 @@ class ATKLaunchController(http.Controller):
         return request.env.user.has_group("base.group_user")
 
     @http.route('/', type='http', auth='public', website=True, sitemap=False)
+    
     def atk_home_router(self, **kw):
+        website = request.website
 
+        # Launch date logic
         if self._launch_is_open():
-            return request.render("theme_atk_navy.atk_homepage")
+            return request.render(
+                "theme_atk_navy.atk_homepage",
+                {'website': website}
+            )
 
         if self._is_admin():
-            return request.render("theme_atk_navy.atk_homepage")
+            return request.render(
+                "theme_atk_navy.atk_homepage",
+                {'website': website}
+            )
 
-        return request.render("theme_atk_navy.atk_countdown")
-    
-    # restart trigger
+        return request.render(
+            "theme_atk_navy.atk_countdown",
+            {'website': website}
+        )
