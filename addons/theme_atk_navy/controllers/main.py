5dfbb682b16539b1ecfe04aa9e23577041372443
gracecreatives <gracecreatives302@gmail.com>
2026-01-09 17:00:52 +0100

    Activate ATK launch controller for homepage routing

--- addons/theme_atk_navy/controllers/main.py
+++ addons/theme_atk_navy/controllers/main.py
@@ -7,29 +7,21 @@ import pytz
 class ATKLaunchController(http.Controller):
 
     def _launch_is_open(self):
-        """Check if launch date has passed (USA time)"""
         usa_tz = pytz.timezone("America/New_York")
         now = datetime.now(usa_tz)
-
-        launch_dt = usa_tz.localize(
-            datetime(2026, 1, 19, 0, 1, 0)
-        )
+        launch_dt = usa_tz.localize(datetime(2026, 1, 19, 0, 1, 0))
         return now >= launch_dt
 
     def _is_admin(self):
-        user = request.env.user
-        return user.has_group("base.group_user")
+        return request.env.user.has_group("base.group_user")
 
-    @http.route("/", type="http", auth="public", website=True)
+    @http.route('/', type='http', auth='public', website=True, sitemap=False)
     def atk_home_router(self, **kw):
-        # If launch is open → everyone sees homepage
+
         if self._launch_is_open():
             return request.render("theme_atk_navy.atk_homepage")
 
-        # Before launch
         if self._is_admin():
-            # Logged-in admin/internal user sees full site
             return request.render("theme_atk_navy.atk_homepage")
 
-        # Public users see countdown
         return request.render("theme_atk_navy.atk_countdown")
