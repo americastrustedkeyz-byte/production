8fd9b62f8a8973834f5056f3c0ec0e02f423d979
gracecreatives <gracecreatives302@gmail.com>
2026-01-10 10:49:38 +0100

    Fix: align homepage and countdown pages as DATA XML

--- addons/atk_website/__manifest__.py
+++ addons/atk_website/__manifest__.py
@@ -1,21 +1,15 @@
 {
     'name': 'ATK Website',
-    'summary': 'Homepage & launch countdown for ATK',
     'version': '18.0.1.0.0',
     'category': 'Website',
-    'author': 'ATK',
-    'license': 'LGPL-3',
-
     'depends': [
         'website',
         'theme_atk_navy',
     ],
-
     'data': [
         'views/homepage_page.xml',
         'views/countdown_page.xml',
     ],
-
     'installable': True,
     'application': False,
 }
