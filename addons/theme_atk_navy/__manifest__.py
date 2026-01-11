2179927fbc9ac5cf59a063688f6e01ae71d8b4ba
gracecreatives <gracecreatives302@gmail.com>
2026-01-11 11:51:34 +0100

    Single Home page logic

--- addons/theme_atk_navy/__manifest__.py
+++ addons/theme_atk_navy/__manifest__.py
@@ -10,8 +10,8 @@
         'views/hero.xml',
         'views/countdown.xml',
     ],
-    'assets': {
-        'web.assets_frontend': [
+        'assets': {
+            'web.assets_frontend': [
             'theme_atk_navy/static/src/scss/atk_theme.scss',
             'theme_atk_navy/static/src/js/countdown.js',
         ],
