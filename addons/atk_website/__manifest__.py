00552176613e00d56eed5c97d17c0f65f01b8efe
gracecreatives <gracecreatives302@gmail.com>
2026-01-11 04:28:18 +0100

    Normalize views: single homepage, remove duplicates

--- addons/atk_website/__manifest__.py
+++ addons/atk_website/__manifest__.py
@@ -1,15 +1,11 @@
 {
-    'name': 'ATK Website',
-    'version': '18.0.1.0.0',
-    'category': 'Website',
-    'depends': [
-        'website',
-        'theme_atk_navy',
+    "name": "ATK Website",
+    "version": "18.0.1.0.0",
+    "depends": ["website", "theme_atk_navy"],
+    "data": [
+        "views/homepage.xml",
+        "views/pages.xml",
+        "views/countdown.xml",
     ],
-    'data': [
-        'views/homepage_view.xml',   # MUST load first
-        'views/pages.xml',
-    ],
-    'installable': True,
-    'application': False,
+    "installable": True,
 }
--- addons/theme_atk_navy/__manifest__.py
+++ addons/theme_atk_navy/__manifest__.py
@@ -1,32 +1,13 @@
 {
-    'name': 'ATK Navy Theme',
-    'description': "America's Trusted Key – Navy Standard Theme",
-    'version': '18.0.1.0.0',
-    'category': 'Theme/Corporate',
-    'author': 'ATK',
-    'license': 'LGPL-3',
-
-    'depends': ['website'],
-
-    #DO NOT put QWeb here
-    'data': [],
-
-    #ALL templates MUST be here
-    'qweb': [
-        'views/navbar.xml',
-        'views/hero.xml',
-        'views/countdown.xml',
+    "name": "ATK Navy Theme",
+    "version": "18.0.1.0.0",
+    "category": "Theme/Corporate",
+    "depends": ["website"],
+    "data": [
+        "views/layout.xml",
+        "views/navbar.xml",
+        "views/hero.xml",
     ],
-
-    'assets': {
-        'web.assets_frontend': [
-            'theme_atk_navy/static/src/scss/atk_theme.scss',
-            'theme_atk_navy/static/src/js/countdown.js',
-            'theme_atk_navy/static/src/scss/navbar.scss',
-        ],
-    },
-
-    'theme': True,
-    'installable': True,
-    'application': False,
+    "theme": True,
+    "installable": True,
 }
