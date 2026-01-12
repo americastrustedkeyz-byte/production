5f544222d7307010b6169bfbc97fc524e84a37fe
gracecreatives <gracecreatives302@gmail.com>
2026-01-12 13:00:03 +0100

    Onboarding bootsrap logic

--- addons/theme_atk_navy/__manifest__.py
+++ addons/theme_atk_navy/__manifest__.py
@@ -14,6 +14,7 @@
         'web.assets_frontend': [
             'theme_atk_navy/static/src/scss/atk_theme.scss',
             'theme_atk_navy/static/src/scss/atk_countdown.scss',
+            'theme_atk_navy/static/src/scss/components/_buttons.scss',
             'theme_atk_navy/static/src/js/atk_countdown.js',
             'theme_atk_navy/static/src/js/atk_navbar.js',
         ],
