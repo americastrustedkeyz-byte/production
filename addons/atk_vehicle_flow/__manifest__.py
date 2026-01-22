aba16ddd8a0095b5434ee5e507795f8dd9b95782
gracecreatives <gracecreatives302@gmail.com>
2026-01-22 13:49:16 +0100

    check redirect v7

--- addons/atk_vehicle_flow/__manifest__.py
+++ addons/atk_vehicle_flow/__manifest__.py
@@ -16,7 +16,7 @@
     'assets': {
         'web.assets_frontend': [
             'atk_vehicle_flow/static/src/scss/booked_slot.scss',
-            #'atk_vehicle_flow/static/src/js/booked_slot.js',
+            'atk_vehicle_flow/static/src/js/booked_slot.js',
             'atk_vehicle_flow/static/src/js/atk_hash_router.js',
             'atk_vehicle_flow/static/src/js/atk_after_booking_redirect.js',
         ],
